061b03a340649150151783bc926fecef3c605ffb
Elvis Ozoria <elvis.ozoria@gmail.com>
2015-04-22 11:00:09 +0200

    mejora en  fullpage js e imagenes
    eliminar full page para la primera seccion, arreglo de imagen de la
    segunda seccion a redimencionarse.
    tingo al portafolio

--- js/main.js
+++ js/main.js
@@ -1,3 +1,53 @@
 $(document).ready(function(){
-	$("#fullpage").fullpage();
+	$("#fullpage").fullpage({
+		
+	easing: 'easeOutBack',
+	autoScrolling:true,
+	scrollBar: true,
+	normalScrollElements: '#section0',
+	scrollingSpeed:1200,
+	fitToSection:false,
+	sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+
+	  afterLoad: function(anchorLink, index){
+            var loadedSection = $(this);
+
+            //using index
+            if(index == 1){
+                $('#introduccion').focus();
+            }
+
+        }
+	});
 });
+
+    
+
+$(window).scroll(function (event) {
+    // Obtener la posicion del scroll
+    var scroll = $(window).scrollTop();
+
+    // si ya paso la primera seccion (400px) pasar a la siguiente
+    if(scroll == 500 ){
+       $.fn.fullpage.moveSectionDown();
+        console.log("estas en "+scroll);
+    }
+}
+);
+
+
+
+$(function(){
+    $('#introduccion').slimScroll({
+        // height: 'auto',
+        height:'auto',
+        allowPageScroll:true
+    }).bind('slimscroll', function(e, pos) {
+        //avanza a la siguiente seccion cuando el scroll llega al fondo del texto
+   	 	// if(pos=="bottom"){
+   	 	// 	$.fn.fullpage.moveSectionDown();
+   	 	// }
+    });
+});
+
+
